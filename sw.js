9a1afe6e1ef78ca008483cf0821721edecd96985
zavalaby16 <zavalaby@gmail.com>
2026-02-25 09:24:01 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,19 @@
 self.skipWaiting();
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          return caches.delete(cache);
-        })
-      );
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          return caches.match("./index.html");
+        });
+      })
   );
 });
 
-const CACHE_NAME = "monkey-runner-v4";
+const CACHE_NAME = "monkey-runner-v5";
 
 const urlsToCache = [
   "./",
