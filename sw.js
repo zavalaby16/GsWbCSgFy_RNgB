348a4c1adcebbced594dd26032f24e0931daba8d
zavalaby16 <zavalaby@gmail.com>
2026-02-25 23:08:13 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,4 @@
-self.skipWaiting();
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).catch(() => {
-          return caches.match("./index.html");
-        });
-      })
-  );
-});
-
-const CACHE_NAME = "GsWbCSgFy_RNgB";
-
+const CACHE_NAME = "monkey-game-v1";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -23,16 +7,38 @@ const urlsToCache = [
   "./icon-512.png"
 ];
 
+// Instalación: Guarda todo en la memoria
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Activación: Borra cachés viejas si cambias el nombre (v1 a v2)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: El corazón del modo offline
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      // Si está en caché, lo devuelve. Si no, lo busca en internet.
+      return response || fetch(event.request).catch(() => {
+        // Si falla internet y no está en caché, intenta devolver el index.html
+        return caches.match("./index.html");
+      });
+    })
   );
 });
